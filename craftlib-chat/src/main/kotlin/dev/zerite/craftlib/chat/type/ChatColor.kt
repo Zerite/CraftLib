beae09cea4746d29a76e70426d4da91f4b1f412a
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-07-12 15:39:49 -0400

    ✨Add int colors to ChatColor

--- craftlib-chat/src/main/kotlin/dev/zerite/craftlib/chat/type/ChatColor.kt
+++ craftlib-chat/src/main/kotlin/dev/zerite/craftlib/chat/type/ChatColor.kt
@@ -8,30 +8,30 @@ package dev.zerite.craftlib.chat.type
  * @author Koding
  * @since  0.1.0-SNAPSHOT
  */
-enum class ChatColor(private val code: Char) {
+enum class ChatColor(val code: Char, val color: Int?) {
 
-    BLACK('0'),
-    DARK_BLUE('1'),
-    DARK_GREEN('2'),
-    DARK_AQUA('3'),
-    DARK_RED('4'),
-    DARK_PURPLE('5'),
-    GOLD('6'),
-    GRAY('7'),
-    DARK_GRAY('8'),
-    BLUE('9'),
-    GREEN('a'),
-    AQUA('b'),
-    RED('c'),
-    LIGHT_PURPLE('d'),
-    YELLOW('e'),
-    WHITE('f'),
-    OBFUSCATED('k'),
-    BOLD('l'),
-    STRIKETHROUGH('m'),
-    UNDERLINE('n'),
-    ITALIC('o'),
-    RESET('r');
+    BLACK('0', 0x000000),
+    DARK_BLUE('1', 0x0000AA),
+    DARK_GREEN('2', 0x00AA00),
+    DARK_AQUA('3', 0x00AAAA),
+    DARK_RED('4', 0xAA0000),
+    DARK_PURPLE('5', 0xAA00AA),
+    GOLD('6', 0xFFAA00),
+    GRAY('7', 0xAAAAAA),
+    DARK_GRAY('8', 0x555555),
+    BLUE('9', 0x5555FF),
+    GREEN('a', 0x55FF55),
+    AQUA('b', 0x55FFFF),
+    RED('c', 0xFF5555),
+    LIGHT_PURPLE('d', 0xFF55FF),
+    YELLOW('e', 0xFFFF55),
+    WHITE('f', 0xFFFFFF),
+    OBFUSCATED('k', null),
+    BOLD('l', null),
+    STRIKETHROUGH('m', null),
+    UNDERLINE('n', null),
+    ITALIC('o', null),
+    RESET('r', null);
 
     /**
      * Prints this formatting code prefixed with the formatting indicator.
